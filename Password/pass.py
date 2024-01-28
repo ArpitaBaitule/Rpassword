62cc630df64308e3b0c5347ef5c5d660d4b88072
ArpitaBaitule <145252639+ArpitaBaitule@users.noreply.github.com>
2024-01-28 17:16:21 +0530

    Add files via upload

---
+++ Password/pass.py
@@ -0,0 +1,40 @@
+import string
+import random
+
+length = int(input("Enter password length: "))
+
+print('''Choose character set for password from these : 
+		1. Digits
+		2. Letters
+		3. Special characters
+		4. Exit''')
+
+character = ""
+
+while(True):
+	choice = int(input("Pick a number  "))
+	if(choice == 1):
+		character += string.digits
+		
+	elif(choice == 2):
+		
+		character += string.ascii_letters
+		
+	elif(choice == 3):
+		
+		character += string.punctuation
+		
+	elif(choice == 4):
+		break
+	else:
+		print("Please pick a valid option!")
+
+password = []
+
+for i in range(length):
+
+	randomchar = random.choice(character)
+	
+	password.append(randomchar)
+
+print("The randomly generated password is " + "".join(password))
